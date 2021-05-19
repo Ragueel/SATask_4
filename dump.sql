1863463f8dca51637c8b6ed4e748251436fa23bd
Ragueel <kayne787@gmail.com>
2021-05-19 22:25:08 +0600

    Init

---
+++ dump.sql
@@ -0,0 +1,101 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.5 (Debian 10.5-2.pgdg90+1)
+-- Dumped by pg_dump version 10.5 (Debian 10.5-2.pgdg90+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    user_id bigint NOT NULL,
+    name character varying(20) NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.users_user_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_user_id_seq OWNER TO postgres;
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.users_user_id_seq OWNED BY public.users.user_id;
+
+
+--
+-- Name: users user_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.users_user_id_seq'::regclass);
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (user_id, name) FROM stdin;
+\.
+
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.users_user_id_seq', 1, false);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (user_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
---
+++ server/app/flaskapp.py
@@ -0,0 +1,80 @@
+from flask import jsonify, request, Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import JSON
+import json
+import os
+
+DB_ADDRESS = os.environ.get('DB_ADDRESS', '192.168.99.100:31792')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', 'master-password')
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:{0}@{1}/postgres".format(DB_PASSWORD,DB_ADDRESS)
+db = SQLAlchemy(app)
+
+class UserModel(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+
+    def toJson(self):
+        return {'name': self.name, 'userId':'user_id'}
+
+
+@app.route("/")
+def index():
+    """Function to test the functionality of the API"""
+    return "Hello, world!"
+
+@app.route('/getAll')
+def get_users():
+    all_users = UserModel.query.all()
+    return json.dumps({'response':[x.toJson() for x in allUsers]})
+
+@app.route('/deleteUser')
+def delete_user():
+    data = request.values.to_dict().keys()[0]
+    data = json.loads(data)
+    found_user = UserModel.query.filter_by(name=data.get('name','')).first()
+
+    if found_user is not None:
+        found_user.name = ''
+        db.session.commit()
+        return {'status': 'Updated'}
+
+    return {'status':'not found'}
+
+@app.route('/updateUser')
+def update_user():
+    data = request.values.to_dict().keys()[0]
+    data = json.loads(data)
+
+    found_user = UserModel.query.filter_by(name=data.get('name','')).first()
+
+    if found_user is not None:
+        found_user.name = ''
+        db.session.commit()
+        return {'status': 'Updated'}
+    return {'status': 'not found'}
+
+@app.route("/create", methods=["POST"])
+def add_user():
+    data = request.values.to_dict().keys()[0]
+    data = json.loads(data)
+
+    new_user = UserModel(name=data.get('name'))
+    db.session.add(new_user)
+
+    db.session.commit()
+    return new_user.toJson()
+
+
+
+if __name__ == "__main__":
+    db.create_all()
+    sample_user = UserModel(name='Sample')
+    db.session.add(sample_user)
+    sample_user = UserModel(name='Sample 2')
+    db.session.add(sample_user)
+    db.session.commit()
+    allUsers = UserModel.query.all()
+
+    app.run(host="0.0.0.0", port=5000)
